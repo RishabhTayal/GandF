9dd8cc61366e833d45f73b2388013de17dbea6cb
Rishabh Tayal <rtayal11@gmail.com>
2014-09-25 14:19:16 -0500

    commit.

--- site/wp-content/themes/nirvana/includes/theme-functions.php
+++ site/wp-content/themes/nirvana/includes/theme-functions.php
@@ -384,8 +384,8 @@ function nirvana_site_info() {
 	$nirvanas = nirvana_get_theme_options();
 	foreach ($nirvanas as $key => $value) { ${"$key"} = $value ; }	?>
 	<em style="display:table;margin:0 auto;float:none;text-align:center;padding:7px 0;font-size:13px;">
-	<?php _e('Powered by','nirvana')?> <a target="_blank" href="<?php echo 'http://www.cryoutcreations.eu';?>" title="<?php echo 'Nirvana Theme by '. 'Cryout Creations';?>"><?php echo 'Nirvana' ?></a> &amp;
-	<a target="_blank" href="<?php echo esc_url('http://wordpress.org/' ); ?>" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'nirvana'); ?>"> <?php printf(' %s.', 'WordPress' ); ?></a></em>
+<!-- 	<?php _e('Powered by','nirvana')?> <a target="_blank" href="<?php echo 'http://www.cryoutcreations.eu';?>" title="<?php echo 'Nirvana Theme by '. 'Cryout Creations';?>"><?php echo 'Nirvana' ?></a> &amp; -->
+<!-- 	<a target="_blank" href="<?php echo esc_url('http://wordpress.org/' ); ?>" title="<?php esc_attr_e('Semantic Personal Publishing Platform', 'nirvana'); ?>"> <?php printf(' %s.', 'WordPress' ); ?></a></em> -->
 	<?php } // nirvana_site_info()
 add_action('cryout_footer_hook','nirvana_site_info',15);
 
